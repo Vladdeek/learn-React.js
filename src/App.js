0a370daa17f69b465d7583825f9a82afdd81a9a8
Vladdeek <vladryazanov2709@gmail.com>
2025-02-04 22:50:49 +0300

    4. Использование состояний

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@ import Header from './components/Header'
 import Image from './img/image.png'
 
 class App extends React.Component{
-    helpText = "help text"
+    constructor(props) {
+        super(props)
+        this.state = {
+            helpText: "help text",
+            userData: ""
+        }
+
+        this.inputclick = this.inputclick.bind(this)
+    }
     render() {
         return (<div className="name">
             <Header title="шапка сайта" name=""/>
-            <h1>{this.helpText}</h1>
-            <input placeholder={this.helpText} onClick={this.inputclick} onMouseEnter={this.mouseover}/>
-            <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+            <h1>{this.state.helpText}</h1>
+            <h2>{this.state.userData}</h2>
+            <input placeholder={this.state.helpText} 
+                onChange={event => this.setState({userData: event.target.value})}
+                onClick={this.inputclick} onMouseEnter={this.mouseover}/>
+            <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
             <img src={Image}/>
         </div>)
     }
-    inputclick() {console.log("click")}
+    inputclick() {
+        
+        this.setState({helpText: "Changed"})
+        console.log("click")
+    }
     mouseover() {console.log("mouse over")}
 }
 
